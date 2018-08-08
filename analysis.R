02817eb96345cc1fdb57dd2046194462cce3a7c5
Matiki <matiasbruzza@gmail.com>
2018-08-08 16:17:35 -0700

    modified code

--- analysis.R
+++ analysis.R
@@ -1,11 +1,22 @@
+# Load necessary R packages
+library(dplyr)
 
 # Set seed
 set.seed(42)
 
 # 1000 simulations of 40 exponential random variables
 exp_sim <- NULL
-for (i in 1:1000){
-        exp_sim <- c(exp_sim, mean(rexp(40, 0.2)))
+num_sims <- 1000
+n <- 40
+lambda <- 0.2
+
+for (i in 1 : num_sims){
+        exp_sim <- c(exp_sim, mean(rexp(n, lambda)))
 }
 
-sim_matrix <- matrix(rexp(40000, 0.2), 1000, 40)
+# Histogram showing the sample distribtution 
+hist(exp_sim)
+
+# Show the sample mean and sample variance (theoretical = 1/lambda)
+mean(exp_sim)
+sd(exp_sim) * sqrt(n)
