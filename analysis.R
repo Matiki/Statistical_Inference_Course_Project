abaabd012f72f100c52a6980ae29e6004c7ca13e
Matiki <matiasbruzza@gmail.com>
2018-08-07 19:28:28 -0700

    test

--- analysis.R
+++ analysis.R
@@ -1,7 +1,11 @@
+
 # Set seed
 set.seed(42)
+
 # 1000 simulations of 40 exponential random variables
 exp_sim <- NULL
 for (i in 1:1000){
         exp_sim <- c(exp_sim, mean(rexp(40, 0.2)))
 }
+
+sim_matrix <- matrix(rexp(40000, 0.2), 1000, 40)
